444269f4bf48e8e5d40cd24d1d5d95c96bc723b2
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2023-08-23 19:15:17 -0400

    Update

--- AlunoBO.java
+++ AlunoBO.java
@@ -1,4 +1,5 @@
 package bo;
+
 import dao.AlunoDAO;
 import javax.swing.*;
 
@@ -8,34 +9,34 @@ import java.awt.*;
 import vo.Aluno;
 
 public class AlunoBO {
-	public void cadastrar(String nomeCompleto, String nomeUsuario, String email,String senha) {
+	public void cadastrar(String nomeCompleto, String nomeUsuario, String email, String senha) {
 		Aluno aluno = new Aluno(nomeCompleto, nomeUsuario, email, senha);
 		AlunoDAO dao = new AlunoDAO();
 		dao.cadastrar(aluno);
 	}
-	
+
 	public boolean consultar(String email, String senha) {
 		AlunoDAO dao = new AlunoDAO();
 		return dao.consultar(email, senha);
 	}
-	
+
 	public boolean atualizar(Aluno aluno, String email, String senha) {
 		AlunoDAO dao = new AlunoDAO();
 
-		if (dao.consultar(email, senha)==false) {
-            return false; // email e senha invalidos, não pode alterar
-        }else {
-    	    return dao.atualizar(aluno);
-        }
+		if (dao.consultar(email, senha) == false) {
+			return false; // email e senha invalidos, não pode alterar
+		} else {
+			return dao.atualizar(aluno);
+		}
 	}
-	
+
 	public boolean remover(String email, String senha) {
 		AlunoDAO dao = new AlunoDAO();
 
-        if (dao.consultar(email, senha)==false) {
-            return false; // email e senha invalidos, não pode apagar
-        }else {
-            return dao.remover(email);
-        }
-    }
+		if (dao.consultar(email, senha) == false) {
+			return false; // email e senha invalidos, não pode apagar
+		} else {
+			return dao.remover(email);
+		}
+	}
 }
